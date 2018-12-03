0916efb9fb572c194aea9ff9447ea9bc778a595b
x1He <x1he@github.com>
2018-12-03 11:09:29 +0800

    two_sum

---
+++ problems/two_sum_001/solution_2.py
@@ -0,0 +1,12 @@
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        dic = {}
+        for index, num in enumerate(nums):
+            if num in dic:
+                return [dic[num], index]
+            dic[target - num] = index
