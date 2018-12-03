7af8522540ce9c77a7f7f459d4bd885fc72fa42a
x1He <x1he@github.com>
2018-12-03 11:55:48 +0800

    palindrome_number

---
+++ problems/palindrome_number_009/solution_1.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x < 0:
+            return False
+        else:
+            return str(x)[::-1] == str(x)
