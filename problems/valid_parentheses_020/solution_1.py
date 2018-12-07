9886972a53b9774134498785a30d101617fc45b1
x1He <x1he@github.com>
2018-12-07 20:01:46 +0800

    20. Valid Parentheses

---
+++ problems/valid_parentheses_020/solution_1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack = []
+        mapping = {')':'(', ']':'[','}':'{'}
+        for i in s:
+            if i in mapping:
+                top = stack.pop() if stack else '*'
+                if mapping[i] != top:
+                    return False
+            else:
+                stack.append(i)
+        return not stack
