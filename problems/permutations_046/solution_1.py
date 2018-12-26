e8fc301cbd8b620afa50ef5dd1fb8fd8714d6fdb
x1He <x1he@github.com>
2018-12-26 16:47:23 +0800

    46. Permutations

--- problems/permutations_046/solution_1.py
+++ problems/permutations_046/solution_1.py
@@ -8,3 +8,10 @@ class Solution:
         for i, num in enumerate(nums):
             res = [line[:j] + [num] + line[j:] for j in range(i+1) for line in res]
         return res
+
+
+if __name__ == "__main__":
+    temp = Solution()
+    nums = [9,8,7]
+    res = temp.permute(nums)
+    print(res)
