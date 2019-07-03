be93ff29b7c74288ab1fbf034e9ae938cb4a94d3
x1He <x1he@github.com>
2019-07-03 14:08:04 +0800

    024

---
+++ problems/swap_nodes_in_pairs_024/__init__.py
@@ -0,0 +1,43 @@
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution(object):
+    def swapPairs(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        pre, pre.next = self, head
+        while pre.next and pre.next.next:
+            a = pre.next
+            b = a.next
+            pre.next, b.next, a.next = b, a, b.next
+            pre = a
+        return self.next
+
+
+if __name__ == '__main__':
+    a = ListNode(1)
+    b = ListNode(2)
+    c = ListNode(3)
+    d = ListNode(4)
+    e = ListNode(5)
+    a.next = b
+    b.next = c
+    c.next = d
+    d.next = e
+    e.next = None
+
+    head = a
+    while head:
+        print(head.val)
+        head = head.next
+    print('----------------------------')
+    t = a
+    z = Solution().swapPairs(t)
+    while z:
+        print(z.val)
+        z = z.next
