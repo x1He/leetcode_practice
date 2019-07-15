f57d8f61a5ef6d7e41a6303dc6a1600e25b9bbfc
x1He <x1he@github.com>
2019-07-15 08:18:39 +0800

    079

--- problems/Word Search_079/__init__.py
+++ problems/Word Search_079/__init__.py
@@ -8,14 +8,15 @@ class Solution(object):
         if not board: return False
         for i in range(len(board)):
             for j in range(len(board[0])):
-                if self._dfs(board, i, j, word):
-                    return True
+                if word[0] == board[i][j]:
+                    if self._dfs(board, i, j, word):
+                        return True
         return False
 
 
     def _dfs(self, board, i, j, word):
         if len(word) == 0: return True
-        if i < 0 or i > len(board) or j < 0 or j > len(board[0]) or word[0]!=board[i][j]:
+        if i < 0 or i >= len(board) or j < 0 or j >= len(board[0]) or word[0]!=board[i][j]:
             return False
         temp = board[i][j]
         board[i][j] = "used"
